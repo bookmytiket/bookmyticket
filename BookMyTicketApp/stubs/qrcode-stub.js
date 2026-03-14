5ab98d3304e4cf556e95ed2113d4a2e618055403
Raja Vasudevan <v.raja2mail@gmail.com>
2026-03-14 12:46:35 +0530

    error fixed

--- BookMyTicketApp/stubs/qrcode-stub.js
+++
@@ -1,8 +0,0 @@
-// Stub for the qrcode npm package on web.
-// react-native-qrcode-svg is NOT web-compatible, so we provide safe empty exports
-// to prevent Metro from crashing with "Utils.getBCHDigit is not a function" etc.
-module.exports = {
-  default: {},
-  create: () => ({ modules: [], version: 0 }),
-  toString: () => '',
-};
--- BookMyTicketApp/stubs/qrcode-utils-stub.js
+++
@@ -1,3 +0,0 @@
-// Stub for qrcode's internal utils module when running on web.
-// react-native-qrcode-svg is not web-compatible, so we provide safe empty exports.
-module.exports = {};
