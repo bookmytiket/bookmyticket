0a7e506139c3a1c66adfbb4e29b55664e4a977c6
Raja Vasudevan <v.raja2mail@gmail.com>
2026-04-20 21:44:19 +0530

    render deployment error fixed

--- middleware.js
+++ middleware.js
@@ -1,23 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const url = request.nextUrl.clone();
-  const hostname = request.headers.get('host');
-
-  // Check if the hostname starts with 'www.'
-  if (hostname && hostname.startsWith('www.')) {
-    // Remove 'www.' from the hostname
-    const newHostname = hostname.replace(/^www\./, '');
-    
-    // Construct the new URL
-    const newUrl = new URL(request.nextUrl.pathname + request.nextUrl.search, `https://${newHostname}`);
-    
-    // Redirect to the non-www version
-    return NextResponse.redirect(newUrl, 301);
-  }
-
   return NextResponse.next();
 }
+
 
 export const config = {
   matcher: [
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  trailingSlash: false,
   images: {
     unoptimized: true,
     remotePatterns: [
